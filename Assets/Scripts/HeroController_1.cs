5d204ac9998e865a588a52ee8237899596bd668b
Ernesto <etorres@amerike.edu.mx>
2022-09-16 22:37:01 -0500

    MetroidVania_PP4
    1.- Saltando infinitas veces
    2.- Realizamos modificaciones en "HeroController_1"

--- Assets/Scripts/HeroController_1.cs
+++ Assets/Scripts/HeroController_1.cs
@@ -8,12 +8,17 @@ public class HeroController_1 : MonoBehaviour
     [Header("Animation Variables")]                                                             //"Pestaña" con título en el Inspector
     [SerializeField] AnimatorController_1 animatorController;               //Instanciamiento de Clase alias "animatorController"
                                                                             //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
+    [SerializeField] private float jumpForce;               //Agregamos una variable flotante para agrear furza al salto
+
 
     [SerializeField] private float speed_;                  //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
     [SerializeField] private Vector2 movementDirection;     //"SerializeField" significa que desde el inspector podemos  manipular o ver su valor.
     private Rigidbody2D rigidbody2D_;                       //Variable de instanciamiento
+    private bool jumpPressed = false;                       //variable usadas para saber si se apretó la barra espaciadora
+                                                            //y es personaje saltó.
 
-                      
+
+
     void Start()
     {
         rigidbody2D_ = GetComponent<Rigidbody2D>();         //Variable de instanciamiento tipo "RigidBody2D"
@@ -28,11 +33,15 @@ public class HeroController_1 : MonoBehaviour
         HandleControls();                                    //invocando el método "HandleControls" (abre el puerto de entrada del teclado)
         HandleMovement();                                    //invocando el método "HandleMovement" (multiplica el valor de "x" por "speed".
         HandleFlip();                                       //invocando el método "HandleFlip" (rota el personaje a la izquierda o a la derecha)
+        HandleJump();                                       //invocando el método "HandleJump" (agregamos furza vertical hacia arriba al Hero)
+
     }
 
     void HandleControls()
     {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        jumpPressed = Input.GetButtonDown("Jump");          //asociamos la variable "jumpPressed" a la barra espaciadora
+
 
     }
 
@@ -65,4 +74,14 @@ public class HeroController_1 : MonoBehaviour
             }
         }
     }
+    void HandleJump()           //Método para agregarle fuerza la RigidBody del Hero
+    {
+
+        if (jumpPressed)        //si la barra espaciadora es apretada.....
+
+        {
+            this.rigidbody2D_.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);//agrega impulso de fuerza instantánea hacia arriba           
+        }
+    }
+
 }
