29e84d7c493ae305ad5ae00a259aee4767f319ac
Ernesto <etorres@amerike.edu.mx>
2022-10-08 21:06:26 -0500

    MetroidVania_PP16
    1.- El texto de Salud puede NO aparecer, debemos de relocalizarlo y cambiar a FullHD el Aspecto del Juego.
    2.- Agregamos un PhysicsMaterial2D para que el Héroe no se atore.
    3.- Agregamos WayPoints y un nuevo Enemigo.

--- Assets/Scripts/Enemy/Aleatorio_2.cs
+++ Assets/Scripts/Enemy/Aleatorio_2.cs
@@ -10,9 +10,9 @@ public class Aleatorio_2 : MonoBehaviour
     {
         Debug.Log(velocidad);
         transform.Translate(velocidad * Time.deltaTime, 0, 0);
-        if (transform.position.x < -9.41f)   //Rango de desplazamieneto en "+X"
+        if (transform.position.x < -11.3f)   //Rango de desplazamieneto en "+X"
             velocidad = -velocidad;
-        if (transform.position.x > -14.29)  //Rango de desplazamieneto en "-X"
+        if (transform.position.x > -25)  //Rango de desplazamieneto en "-X"
             velocidad = -velocidad;
     }
 }
--- Assets/Scripts/Enemy/Aleatorio_4.cs
+++ Assets/Scripts/Enemy/Aleatorio_4.cs
@@ -10,9 +10,9 @@ public class Aleatorio_4 : MonoBehaviour
     {
         Debug.Log(velocidad);
         transform.Translate(velocidad * Time.deltaTime, 0, 0);
-        if (transform.position.x < 3.29)   //Rango de desplazamieneto en "+X"
+        if (transform.position.x < 3.00)   //Rango de desplazamieneto en "+X"
             velocidad = -velocidad;
-        if (transform.position.x > -6.71)  //Rango de desplazamieneto en "-X"
+        if (transform.position.x > -6.0)  //Rango de desplazamieneto en "-X"
             velocidad = -velocidad;
     }
 }
