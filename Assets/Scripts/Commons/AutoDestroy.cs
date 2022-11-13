2b69f16bc8ee0f8c233edd3ebefab7c533776e1d
Ernesto <etorres@amerike.edu.mx>
2022-11-13 10:22:00 -0600

    MetroidVania_PP19
    1,. Se reacomodó lo mejor posible las carpetas
    2.- Creamos GO Tourch
    3.- Creamos GO EnemyExplotion
    3.- Creamos Explosiones,
    4.- Usamos los Scripts "tourchController.cs" ,  "AutoDestry.sc".

---
+++ Assets/Scripts/Commons/AutoDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    [SerializeField] float delay = 3;
+    [SerializeField] AudioClip sfx;
+
+   
+    private void Awake()
+    {
+        if (sfx) {
+            AudioManager_1.instance.PlaySfx(sfx);
+        }
+        Destroy(this.gameObject, delay);
+    }
+}
---
+++ Assets/Scripts/PowerUps/Tourch/TorchController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour, ITargetCombat_1
+{
+
+    [SerializeField] GameObject destructionEffect;
+    [SerializeField] AudioClip destructionSfx;
+
+
+    //[SerializeField] GameObject coin;
+    //[SerializeField] GameObject heart;
+
+    //[SerializeField] List<GameObject> powerUps;
+
+    //public bool powerUpsOnly;
+    //public bool indestructible = false;
+    public void TakeDamage(int damagePoints)
+    {
+
+        Instantiate(destructionEffect, this.transform.position, Quaternion.identity);
+        GameObject prefabToInstance = null;
+/*
+        if (!powerUpsOnly)
+        {
+            if (Random.Range(-10, 10) > 0)
+            {
+                if (Random.Range(-10, 10) > 0)
+                {
+                    prefabToInstance = coin;
+                }
+                else
+                {
+                    if (Random.Range(-10, 10) > 0)
+                    {
+                        prefabToInstance = heart;
+                    }
+                  
+
+                }
+
+            }
+        }
+        else {
+
+            if (Random.Range(-10, 10) > 0)
+            {
+                prefabToInstance = powerUps[Random.Range(0, powerUps.Count)];
+            }
+        }
+*/
+        if (prefabToInstance != null) {
+            Instantiate(prefabToInstance, this.transform.position, Quaternion.identity);
+
+        }
+
+        AudioManager_1.instance.PlaySfx(destructionSfx);
+
+        //if(!indestructible)
+         Destroy(this.gameObject);
+
+    }
+}
